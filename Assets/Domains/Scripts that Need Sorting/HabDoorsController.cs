896d1fcb0afd7d5fd598ae5e1ffcbf86db9d2618
Ben Shapiro <bshap93@gmail.com>
2025-04-04 14:06:39 -0600

    Organized scripts and added Quest Machine

--- Assets/Domains/Scripts that Need Sorting/HabDoorsController.cs
+++ Assets/Domains/Scripts that Need Sorting/HabDoorsController.cs
@@ -1,59 +1,60 @@
-using System;
 using DG.Tweening;
 using JetBrains.Annotations;
 using MoreMountains.Feedbacks;
 using UnityEngine;
 
-public class HabDoorsController : MonoBehaviour
+namespace Domains.Scripts_that_Need_Sorting
 {
-    private bool isOpen = false;
-
-    public GameObject RDoor;
-    public GameObject LDoor;
-
-    private Quaternion originalRotationR;
-    private Quaternion originalRotationL;
-
-    [CanBeNull] public MMFeedbacks OpenDoorFeedbacks;
-    [CanBeNull] public MMFeedbacks CloseDoorFeedbacks;
-
-    private void Start()
+    public class HabDoorsController : MonoBehaviour
     {
-        // Store original rotations on Start
-        originalRotationR = RDoor.transform.rotation;
-        originalRotationL = LDoor.transform.rotation;
-    }
+        public GameObject RDoor;
+        public GameObject LDoor;
 
-    public void OpenDoors()
-    {
-        if (isOpen) return;
-        isOpen = true;
-        if (OpenDoorFeedbacks != null) OpenDoorFeedbacks.PlayFeedbacks();
-        
-        RDoor.transform.DORotate(originalRotationR.eulerAngles + new Vector3(0, -90, 0), 1);
-        LDoor.transform.DORotate(originalRotationL.eulerAngles + new Vector3(0, 90, 0), 1);
-    }
+        [CanBeNull] public MMFeedbacks OpenDoorFeedbacks;
+        [CanBeNull] public MMFeedbacks CloseDoorFeedbacks;
+        private bool isOpen;
+        private Quaternion originalRotationL;
 
-    public void CloseDoors()
-    {
-        if (!isOpen) return;
-        isOpen = false;
-        if (CloseDoorFeedbacks != null) CloseDoorFeedbacks.PlayFeedbacks();
+        private Quaternion originalRotationR;
 
-        // Reset to original rotation instead of arbitrary values
-        RDoor.transform.DORotate(originalRotationR.eulerAngles, 1);
-        LDoor.transform.DORotate(originalRotationL.eulerAngles, 1);
-    }
+        private void Start()
+        {
+            // Store original rotations on Start
+            originalRotationR = RDoor.transform.rotation;
+            originalRotationL = LDoor.transform.rotation;
+        }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-            OpenDoors();
-    }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+                OpenDoors();
+        }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-            CloseDoors();
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+                CloseDoors();
+        }
+
+        public void OpenDoors()
+        {
+            if (isOpen) return;
+            isOpen = true;
+            if (OpenDoorFeedbacks != null) OpenDoorFeedbacks.PlayFeedbacks();
+
+            RDoor.transform.DORotate(originalRotationR.eulerAngles + new Vector3(0, -90, 0), 1);
+            LDoor.transform.DORotate(originalRotationL.eulerAngles + new Vector3(0, 90, 0), 1);
+        }
+
+        public void CloseDoors()
+        {
+            if (!isOpen) return;
+            isOpen = false;
+            if (CloseDoorFeedbacks != null) CloseDoorFeedbacks.PlayFeedbacks();
+
+            // Reset to original rotation instead of arbitrary values
+            RDoor.transform.DORotate(originalRotationR.eulerAngles, 1);
+            LDoor.transform.DORotate(originalRotationL.eulerAngles, 1);
+        }
     }
 }
