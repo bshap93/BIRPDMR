235ca79900bd4367e47bc0304de989ca3c299927
Ben Shapiro <bshap93@gmail.com>
2025-04-13 14:09:29 -0600

    Small improvements

--- Assets/Domains/Player/Scripts/CharacterResourcePaths.cs
+++ Assets/Domains/Player/Scripts/CharacterResourcePaths.cs
@@ -6,7 +6,8 @@ namespace Domains.Player.Scripts
     public static class CharacterResourcePaths
     {
         public static string CharacterStatProfileFilePath =>
-            "CharacterStatsProfiles/MediumProfile";
+            // "CharacterStatsProfiles/MediumProfile";
+            "CharacterStatsProfiles/BasicProfile";
 
         public static string GameLevelStatProfileFilePath =>
             "GameLevelStatsProfiles/MainSceneLevelStats";
