13a9732b641207f66282cb66774979917e70dd2c
Ben Shapiro <bshap93@gmail.com>
2025-04-13 14:46:19 -0600

    Added biocontainers

--- Assets/Domains/Scene/Terrain/Scripts/TerrainBehavior.cs
+++ Assets/Domains/Scene/Terrain/Scripts/TerrainBehavior.cs
@@ -12,9 +12,18 @@ namespace Domains.Scene.Terrain.Scripts
         public int terrainToUseInstead; // or whatever type you need
     }
 
+    [Serializable]
+    public class DefaultLayerAboveDepth
+    {
+        public float playerDepth;
+        public int defaultLayerIndex;
+        public int[] alternateAcceptableLayerIndices;
+    }
+
     [CreateAssetMenu(fileName = "TerrainBehavior", menuName = "Scriptable Objects/TerrainBehavior")]
     public class TerrainBehavior : ScriptableObject
     {
         [FormerlySerializedAs("statEntries")] public List<TerrainLayerChoices> terrainChoices;
+        public List<DefaultLayerAboveDepth> defaultLayerAboveDepths;
     }
 }
