55b2fa8c99f9f428da2fc58c549941b9c637c1ab
Ben Shapiro <bshap93@gmail.com>
2025-04-17 15:19:02 -0600

    Pausing pauses the right audio sources

--- Assets/Domains/Gameplay/Managers/Scripts/PauseManager.cs
+++ Assets/Domains/Gameplay/Managers/Scripts/PauseManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Domains.Input.Scripts;
 using Domains.Scene.Events;
 using UnityEngine;
@@ -6,6 +7,9 @@ namespace Domains.Gameplay.Managers.Scripts
 {
     public class PauseManager : MonoBehaviour
     {
+        [SerializeField] private AudioSource uiButtonAudioSource;
+        private List<AudioSource> _audioSources = new();
+
         private bool _isPaused;
 
         private void Update()
@@ -14,7 +18,19 @@ namespace Domains.Gameplay.Managers.Scripts
             {
                 _isPaused = !_isPaused;
                 Time.timeScale = _isPaused ? 0 : 1;
-                AudioListener.pause = _isPaused;
+                if (_isPaused)
+                {
+                    _audioSources = new List<AudioSource>(FindObjectsByType<AudioSource>(FindObjectsSortMode.None));
+                    foreach (var audioSource in _audioSources)
+                        if (audioSource != null && audioSource != uiButtonAudioSource)
+                            audioSource.Pause();
+                }
+                else
+                {
+                    foreach (var audioSource in _audioSources)
+                        if (audioSource != null && audioSource != uiButtonAudioSource)
+                            audioSource.UnPause();
+                }
 
 
                 SceneEvent.Trigger(SceneEventType.TogglePauseScene);
